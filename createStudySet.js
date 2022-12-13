4e950a08a528b483a8c5a5836ef9a9b22b06c22a
Ehan Ahamed <ehanahamed@ehan.dev>
2022-12-13 18:05:01 -0500

    Crerate basic JS structure for create page

---
+++ createStudySet.js
@@ -0,0 +1,13 @@
+/**
+ * Quizlate (https://quizlate.ehan.dev)
+ *
+ * Copyright (c) 2022 Ehan Ahamed and contributors
+ * Licensed under the UPL-1.0 License
+ * See license file: https://projects.ehan.dev/Quizlate/LICENSE.txt
+**/
+
+var createStudySet = {
+  set: {},
+  ui: {}
+}
+
