5c5aca3e319530c608f0416823aff4b6938f8ccb
Ehan <ehanahamed@ehan.dev>
2022-12-19 08:29:57 -0500

    work in progress

--- downloader.js
+++ downloader.js
@@ -1,9 +1,9 @@
 /*
-downloadstuff work in progres thingy (https://quizlate.ehan.dev)
+simpleBlobLibrary (https://simpleBlobLibrary.ehan.dev)
 
 Copyright (c) 2022 Ehan Ahamed and contributors
 Licensed under the UPL-1.0 License
-See license file: https://projects.ehan.dev/Quizlate/LICENSE.txt
+See license file: https://projects.ehan.dev/simpleBlobLibrary/LICENSE.txt
 */
 
 var downloader = {
@@ -12,9 +12,16 @@ var downloader = {
   },
   makeBlobUrl: function (content, mimetype, filename) {
     return URL.createObjectURL(new Blob([content], { type: mimetype }));
+  },
+  mimetypes: {
+    
   }
 }
 
 // this is the mimetype for json btw: application/json
 // use attribute "href" set to downloadUrl (downloadUrl is returned by this function)
 // use attribute "download" set to filename, use attribute on download button ("a" tag)
+
+// also see
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+// https://iana.org/assignments/media-types/media-types.xhtml
