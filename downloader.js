3ef6c74f8562f54634a2acd3da199db6ee6d2afb
Ehan <ehanahamed@ehan.dev>
2022-12-20 14:58:01 +0000

    its not important enough to be it's own library

--- downloader.js
+++ downloader.js
@@ -1,12 +1,12 @@
 /*
-simpleBlobLibrary (https://simpleBlobLibrary.ehan.dev)
+Quizlate (https://quizlate.ehan.dev)
 
 Copyright (c) 2022 Ehan Ahamed and contributors
 Licensed under the UPL-1.0 License
-See license file: https://projects.ehan.dev/simpleBlobLibrary/LICENSE.txt
+See license file: https://projects.ehan.dev/Quizlate/LICENSE.txt
 */
 
-var downloader = {
+var download = {
   makeBlob: function (content, mimetype) {
     return new Blob([content], { type: mimetype })
   },
