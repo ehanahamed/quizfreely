f2820ee63f17cd50b8f10bbe69e083dc19851e2e
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-06 21:26:02 +0000

    Update 'site/downloader.js'

--- site/downloader.js
+++ site/downloader.js
@@ -1,6 +1,5 @@
 /*!
 Quizfreely (https://quizfreely.ehan.dev)
-
 Copyright (c) 2022 Ehan Ahamed and contributors
 Licensed under the UPL-1.0 License
 See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
