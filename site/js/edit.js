7065ae06335a931ca8d067feeaa24a8402ae09c1
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-06 22:48:48 +0000

    Update 'site/js/edit.js'

--- site/js/edit.js
+++ site/js/edit.js
@@ -2,11 +2,11 @@
 function saveTable(id) {
     tableRows = document.getElementById(id).rows
     tableData = []
-    for (var i = 0; i < tableRows.length; i++) {
+    for (var i = 1; i < tableRows.length - 1; i++) {
         rows = tableRows[i].children
         row = []
         for (var i2 = 0; i2 < rows.length; i2++) {
-            row.push(rows[i2].textContent);
+            row.push(rows[i2].children[0].innerHTML);
         }
         tableData.push(row)
     }
