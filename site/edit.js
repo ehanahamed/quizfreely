1dcd99f5539cae05f86e798f0e78259095a732ce
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-06 19:05:50 +0000

    Update 'site/edit.js'

--- site/edit.js
+++ site/edit.js
@@ -1,9 +1,14 @@
 /* save html table content into js array */
 function saveTable(id) {
-    table = document.getElementById(id);
-    data = [];
-    for (var i = 0; i < table.rows.length; i++) {
-        console.log(table.rows[i]);
+    tableRows = document.getrowsementById(id).rows
+    tableData = []
+    for (var i = 0; i < tableRows.length; i++) {
+        rows = tableRows[i].children
+        row = []
+        for (var i2 = 0; i2 < rows.length; i2++) {
+            row.push(rows[j].innerText);
+        }
+        tableData.push(row)
     }
-    return data;
+    return tableData
 }
