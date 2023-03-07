41d8c61c8481011e7b3486110d9a7a0c41fa082f
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-07 01:58:41 +0000

    Update 'site/js/studySet.js'

--- site/js/studySet.js
+++ site/js/studySet.js
@@ -1,6 +1,6 @@
 /*!
 Quizfreely (https://quizfreely.ehan.dev/)
-Copyright (c) 2022 Ehan Ahamed and contributors
+Copyright (c) 2022-2023 Ehan Ahamed and contributors
 Licensed under the UPL-1.0 License
 See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
 */
