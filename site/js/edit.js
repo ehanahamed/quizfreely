ded8a5454d0df82c5ef225bc556e30e8e15d4b3d
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-07 01:58:52 +0000

    Update 'site/js/edit.js'

--- site/js/edit.js
+++ site/js/edit.js
@@ -1,3 +1,9 @@
+/*!
+Quizfreely (https://quizfreely.ehan.dev/)
+Copyright (c) 2022-2023 Ehan Ahamed and contributors
+Licensed under the UPL-1.0 License
+See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
+*/
 
 function editAddButton() {
     var table = document.getElementById("editTable");
