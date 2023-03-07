ed32ebabfa33bc6ab7b3781626751523a145a200
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-07 15:03:04 +0000

    Update 'site/js/uploader.js'

--- site/js/uploader.js
+++ site/js/uploader.js
@@ -6,20 +6,5 @@ See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
 */
 
 var uploader = {
-  /*
-const [file] = document.querySelector("input[type=file]").files;
-const reader = new FileReader();
-
-reader.addEventListener(
-  "load",
-  () => {
-    // this will then display a text file
-    content.innerText = reader.result;
-  },
-  false
-);
-
-if (file) {
-  reader.readAsText(file);
+  // take from ehanahamed/src
 }
-*/
