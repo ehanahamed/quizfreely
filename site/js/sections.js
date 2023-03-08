4667250153017d777e11bb7ce65a7097ef32b44c
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-08 16:57:08 +0000

    Update 'site/js/sections.js'

--- site/js/sections.js
+++ site/js/sections.js
@@ -6,10 +6,28 @@ See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
 */
 
 var sections = {
-    importButtonOnClick: function () {
-        sectionLoad.style.display = "none";
-        sectionImport.style.display = "block";
+    init: function () {
+        var sectionLoad = document.getElementById("load");
+        var sectionImport = document.getElementById("import");
+        var sectionImportLocal = document.getElementById("importLocal")
+        var sectionEdit = document.getElementById("edit");
+        var sectionExport = document.getElementById("export");
+        
+        sectionLoad.style.display = "block";
+        sectionImport.style.display = "none";
+        sectionImportLocal.style.display = "none";
         sectionEdit.style.display = "none";
         sectionExport.style.display = "none";
+    },
+    buttons: {
+        create: function () {
+
+        },
+        import: function () {
+            sectionLoad.style.display = "none";
+            sectionImport.style.display = "block";
+            sectionEdit.style.display = "none";
+            sectionExport.style.display = "none";
+        }
     }
 }
