a59f6fc4a7d9bfb04e5532f79d36e282881d3c9c
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-08 05:44:58 +0000

    Update 'site/js/edit.js'

--- site/js/edit.js
+++ site/js/edit.js
@@ -9,57 +9,53 @@ var studySetData = {};
 
 var sectionLoad = document.getElementById("load");
 var sectionImport = document.getElementById("import");
+var sectionImportLocal = document.getElementById("importLocal");
 var sectionEdit = document.getElementById("edit");
 var sectionExport = document.getElementById("export");
 
 var editName = document.getElementById("editName");
 
-function createButton() {
-    studySetData = studySet.create();
+var edit = {
+    createButtonOnClick: function () {
+        studySetData = studySet.create();
 
-    sectionLoad.style.display = "none";
-    sectionImport.style.display = "none";
-    sectionEdit.style.display = "block";
-    sectionExport.style.display = "none";
-};
+        sectionLoad.style.display = "none";
+        sectionImport.style.display = "none";
+        sectionEdit.style.display = "block";
+        sectionExport.style.display = "none";
+    },
 
-function importButton() {
-    sectionLoad.style.display = "none";
-    sectionImport.style.display = "block";
-    sectionEdit.style.display = "none";
-    sectionExport.style.display = "none";
-}
+    editAddButton: function (id) {
+        var table = document.getElementById(id);
+        var row = table.insertRow(table.rows.length - 1);
+        var cellTerm = row.insertCell(0);
+        var cellDef = row.insertCell(1);
+        cellTerm.innerHTML = "<input type='text' placeholder='Term'></input>";
+        cellDef.innerHTML = "<textarea rows='2' placeholder='Definition'></textarea>";
+    },
 
-function editAddButton() {
-    var table = document.getElementById("editTable");
-    var row = table.insertRow(table.rows.length - 1);
-    var cellTerm = row.insertCell(0);
-    var cellDef = row.insertCell(1);
-    cellTerm.innerHTML = "<input type='text' placeholder='Term'></input>";
-    cellDef.innerHTML = "<textarea rows='2' placeholder='Definition'></textarea>";
-}
-
-function getTableData(id) {
-    tableRows = document.getElementById(id).rows
-    tableData = []
-    for (var i = 1; i < tableRows.length - 1; i++) {
-        rows = tableRows[i].children
-        row = []
-        for (var i2 = 0; i2 < rows.length; i2++) {
-            row.push(rows[i2].children[0].value);
+    getTableData: function (id) {
+        tableRows = document.getElementById(id).rows
+        tableData = []
+        for (var i = 1; i < tableRows.length - 1; i++) {
+            rows = tableRows[i].children
+            row = []
+            for (var i2 = 0; i2 < rows.length; i2++) {
+                row.push(rows[i2].children[0].value);
+            }
+            tableData.push(row)
         }
-        tableData.push(row)
+        return tableData
+    },
+
+    editDone: function () {
+        studySetData.name = editName.value;
+        studySetData.set = getTableData("editTable");
+        console.log(studySetData);
+
+        sectionLoad.style.display = "none";
+        sectionImport.style.display = "none";
+        sectionEdit.style.display = "none";
+        sectionExport.style.display = "block";
     }
-    return tableData
-}
-
-function editDone() {
-    studySetData.name = editName.value;
-    studySetData.set = getTableData("editTable");
-    console.log(studySetData);
-
-    sectionLoad.style.display = "none";
-    sectionImport.style.display = "none";
-    sectionEdit.style.display = "none";
-    sectionExport.style.display = "block";
 }
