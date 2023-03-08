b66a9eab3a75fb657579bb4e4519c9030a8cbb68
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-08 05:46:01 +0000

    Update 'site/js/sections.js'

--- site/js/sections.js
+++ site/js/sections.js
@@ -4,3 +4,12 @@ Copyright (c) 2022-2023 Ehan Ahamed and contributors
 Licensed under the UPL-1.0 License
 See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
 */
+
+var sections = {
+    importButton: function () {
+        sectionLoad.style.display = "none";
+        sectionImport.style.display = "block";
+        sectionEdit.style.display = "none";
+        sectionExport.style.display = "none";
+    }
+}
