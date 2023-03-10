b31b5e1aa8a59db658f47fb3dd596f8d7d1191e2
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-10 00:35:02 +0000

    Add 'site/js/elements.js'

---
+++ site/js/elements.js
@@ -0,0 +1,20 @@
+/*!
+Quizfreely (https://quizfreely.ehan.dev/)
+Copyright (c) 2022-2023 Ehan Ahamed and contributors
+Licensed under the UPL-1.0 License
+See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
+*/
+
+var elements = {
+    sections: {
+        load: document.getElementById("load"),
+        importOptions: document.getElementById("importOptions"),
+        importLocal: document.getElementById("importLocal"),
+        edit: document.getElementById("edit"),
+        exportOptions: document.getElementById("export"),
+    },
+    buttons: {
+        create: document.getElementById("create"),
+        importOptions: document.getElementById()
+    }
+}
