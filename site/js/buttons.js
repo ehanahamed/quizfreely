00bfc36e83d1dd1a678c28d7859e81edb9ff4fd0
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-10 15:01:00 +0000

    Add 'site/js/buttons.js'

---
+++ site/js/buttons.js
@@ -0,0 +1,15 @@
+/*!
+Quizfreely (https://quizfreely.ehan.dev/)
+Copyright (c) 2022-2023 Ehan Ahamed and contributors
+Licensed under the UPL-1.0 License
+See license file: https://src.ehan.dev/quizfreely/LICENSE.txt
+*/
+
+var buttons = {
+    create: function() {
+
+    },
+    importOptions: function () {
+
+    }
+}
