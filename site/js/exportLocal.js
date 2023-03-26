f2d15a766efbd024cfba0db028bb54d54811dee4
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-26 05:43:58 +0000

    Update 'site/js/exportLocal.js'

--- site/js/exportLocal.js
+++ site/js/exportLocal.js
@@ -7,8 +7,8 @@ https://src.ehan.dev/quizfreely/LICENSE.txt
 
 var exportLocal = {
   save: function () {
-    elements.links.exportLocal.download.href = exportLocal.makeBlobUrl(JSON.stringify(studySetData), "application/json");
-    elements.links.exportLocal.download.download = studySetData.name + ".json"
+    elements.links.exportLocal.download.href = exportLocal.makeBlobUrl(JSON.stringify(sessionData.studySetData), "application/json");
+    elements.links.exportLocal.download.download = sessionData.studySetData.name + ".json"
   },
   makeBlobUrl: function (content, mimetype) {
     return URL.createObjectURL(new Blob([content], { type: mimetype }));
