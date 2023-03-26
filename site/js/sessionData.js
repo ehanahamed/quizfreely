8b6972cd36a8e509a57a6591cc32f95d58250297
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-26 05:35:56 +0000

    Update 'site/js/sessionData.js'

--- site/js/sessionData.js
+++ site/js/sessionData.js
@@ -1,3 +1,9 @@
+/*!
+Quizfreely (quizfreely.ehan.dev)
+Copyright (c) 2022-present Ehan Ahamed and contributors
+Licensed under the UPL-1.0 License
+https://src.ehan.dev/quizfreely/LICENSE.txt
+*/
 var sessionData = {
     importLocal: {},
     edit: {}
