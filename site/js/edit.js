f3ba802f1220815c575a7622a5a4c0ebb7b7bc93
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-27 14:21:04 +0000

    big brain time

--- site/js/edit.js
+++ site/js/edit.js
@@ -31,28 +31,43 @@ var edit = {
         for (var i = 1; i < element.rows.length - 1; i++) {
             var row = [];
             var tableCells = element.rows[i].children;
+            
+            /* code below is for only two columns */
+            row.push(tableCells[0].children[0].value);
+            row.push(tableCells[1].children[0].value);
+            
+            /* code below is for infinite columns */
+            /*
             for (var i2 = 0; i2 < tableCells.length; i2++) {
                 row.push(tableCells[i2].children[0].value);
             }
-            tableArray.push(row)
+            */
+
+            tableArray.push(row);
         }
-        return tableArray
+        return tableArray;
     },
     makeTableFromArray: function (array, table) {
         for (var i = 0; i < array.length; i++) {
-            var row = array[i]
+            var row = array[i];
             var newRow = table.insertRow(table.rows.length - 1);
+
+            /* code below is for only two columns */
+            var newCell = newRow.insertCell(0);
+            newCell.innerHTML = "<input type='text' placeholder='Term'></input>";
+            newCell.children[0].value = row[0];
+            var newCell = newRow.insertCell(1);
+            newCell.innerHTML = "<textarea rows='2' placeholder='Definition'></textarea>";
+            newCell.children[0].value = row[1];
+
+            /* code below is for infinite columns*/
+            /*
             for (var i2 = 0; i2 < row.length; i2++) {
-                if (i2 === 0) {
-                    var newCell = newRow.insertCell(i2)
-                    newCell.innerHTML = "<input type='text' placeholder='Term'></input>";
-                    newCell.children[0].value = row[i2];
-                } else {
-                    var newCell = newRow.insertCell(i2)
-                    newCell.innerHTML = "<textarea rows='2' placeholder='Definition'></textarea>";
-                    newCell.children[0].value = row[i2];
-                }
+                var newCell = newRow.incertCell(i2);
+                newCell.innerHTML = "html goes here";
+                newCell.children[0].value = row[0];
             }
+            */
         }
     },
 }
