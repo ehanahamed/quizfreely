1331334bd1504a46d46c04e56b53c1f57b3a326b
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-27 04:25:43 +0000

    Update 'site/js/flashcards.js'

--- site/js/flashcards.js
+++ site/js/flashcards.js
@@ -7,10 +7,18 @@ https://src.ehan.dev/quizfreely/LICENSE.txt
 
 var flashcards = {
     load: function (index) {
-        elements.flashcards.cardFront.innerText = sessionData.studySetData.data[index][0];
-        elements.flashcards.cardBack.innerText = sessionData.studySetData.data[index][1];
+        elements.flashcards.cardFront.innerText = sessionData.studySetData.data[sessionData.flashcards.index][0];
+        elements.flashcards.cardBack.innerText = sessionData.studySetData.data[sessionData.flashcards.index][1];
     },
     flip: function () {
         elements.flashcards.card.classList.toggle("flip");
+    },
+    next: function () {
+        sessionData.flashcards += 1
+        flashcards.load(sessionData.flashcards.index);
+    },
+    prev: function () {
+        sessionData.flashcards -= 1
+        flashcards.load(sessionData.flashcards.index);
     }
 }
