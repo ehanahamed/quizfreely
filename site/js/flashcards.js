2e8905fb43281f527aa4cc38c0c6293be9900685
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-27 04:13:35 +0000

    Update 'site/js/flashcards.js'

--- site/js/flashcards.js
+++ site/js/flashcards.js
@@ -6,6 +6,10 @@ https://src.ehan.dev/quizfreely/LICENSE.txt
 */
 
 var flashcards = {
+    load: function (index) {
+        elements.flashcards.cardFront.innerText = sessionData.studySetData.data[index][0];
+        elements.flashcards.cardBack.innerText = sessionData.studySetData.data[index][1];
+    },
     flip: function () {
         elements.flashcards.card.classList.toggle("flip");
     }
