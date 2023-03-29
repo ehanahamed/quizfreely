035c015fcfccd98eb48c895f6ba728be172d087b
Ehan Ahamed <ehanahamed@ehan.dev>
2023-03-29 02:06:31 +0000

    make the birds extinct with one stone

--- site/js/edit.js
+++ site/js/edit.js
@@ -18,12 +18,15 @@ var edit = {
         var newRow = elements.inputs.edit.table.insertRow(elements.inputs.edit.table.rows.length - 1);
         newRow.insertCell(0).innerHTML = "<input type='text' placeholder='Term'></input>";
         newRow.insertCell(1).innerHTML = "<textarea rows='2' placeholder='Definition'></textarea>";
-        newRow.insertCell(2).innerHTML = "<button onclick='edit.remove("+newIndex+")'>X ("+newIndex+")</button>";
+        newRow.insertCell(2).innerHTML = "<button onclick='edit.remove("+newIndex+")'>X</button>";
     },
     save: function () {
             sessionData.studySetData.name = elements.inputs.edit.name.value;
             sessionData.studySetData.data = edit.makeArrayFromTable(elements.inputs.edit.table);
             exportLocal.save();
+    },
+    remove: function (index) {
+        elements.inputs.edit.table.deleteRow(index);
     },
     makeArrayFromTable: function (element) {
         var tableArray = [];
@@ -31,16 +34,8 @@ var edit = {
             var row = [];
             var tableCells = element.rows[i].children;
             
-            /* code below is for only two columns */
             row.push(tableCells[0].children[0].value);
             row.push(tableCells[1].children[0].value);
-            
-            /* code below is for infinite columns */
-            /*
-            for (var i2 = 0; i2 < tableCells.length; i2++) {
-                row.push(tableCells[i2].children[0].value);
-            }
-            */
 
             tableArray.push(row);
         }
@@ -49,24 +44,11 @@ var edit = {
     makeTableFromArray: function (array, table) {
         for (var i = 0; i < array.length; i++) {
             var row = array[i];
-            var newRow = table.insertRow(table.rows.length - 1);
+            
+            edit.add();
 
-            /* code below is for only two columns */
-            var newCell = newRow.insertCell(0);
-            newCell.innerHTML = "<input type='text' placeholder='Term'></input>";
-            newCell.children[0].value = row[0];
-            var newCell = newRow.insertCell(1);
-            newCell.innerHTML = "<textarea rows='2' placeholder='Definition'></textarea>";
-            newCell.children[0].value = row[1];
-
-            /* code below is for infinite columns*/
-            /*
-            for (var i2 = 0; i2 < row.length; i2++) {
-                var newCell = newRow.incertCell(i2);
-                newCell.innerHTML = "html goes here";
-                newCell.children[0].value = row[0];
-            }
-            */
+            table.rows[i + 1].children[0].children[0].value = row[0]
+            table.rows[i + 1].children[1].children[0].value = row[1]
         }
     },
 }
