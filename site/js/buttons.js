6e1c31498b66faf99e672f1df5bb5b81f5c53cf7
Ehan Ahamed <ehanahamed@ehan.dev>
2023-07-22 15:30:22 -0400

    problem solveded

--- site/js/buttons.js
+++ site/js/buttons.js
@@ -54,6 +54,16 @@ var buttons = {
           sessionData.studySetData.data[i][1];
       }
       document.getElementById("mainActionsSave").classList.remove("hide");
+      /* IMPORTANT:
+        after the screen switches to sections.changeTo("open") and ...mainActionsSave").classList.remove("hide") (the code above),
+        the edit section will still have the current study set inside of its table, it will just be hidden,
+        if the user goes back to the edit screen without refreshing, it will load the study set again, and the table will be duplicated
+
+        to prevent this,
+        the code below clears/resets the table
+      */
+      document.getElementById("inputEditTable").innerHTML = "<thead> <tr> <th>Term</th> <th>Definition</th> <th>Actions</th> </tr> </thead> <tbody> <tr> <td> <div class='flex'> <button onclick='buttons.edit.add();'> <i class='nf nf-oct-plus'></i> Add row </button> <button id='buttonEditDone' class='alt' onclick='buttons.edit.done()' > <i class='nf nf-oct-check'></i> Done </button> </div> </td> <td></td> <td></td> </tr> </tbody>"
+      /* the string above has the inner html of the table found in dashboard.html */
     },
   },
   save: {},
