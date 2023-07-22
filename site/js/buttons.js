2e9903b6e06d1bd9cd8d45ac59f5f5864f205c67
Ehan Ahamed <ehanahamed@ehan.dev>
2023-07-22 15:48:39 -0400

    kein problem

--- site/js/buttons.js
+++ site/js/buttons.js
@@ -63,7 +63,13 @@ var buttons = {
         the code below clears/resets the table
       */
       document.getElementById("inputEditTable").innerHTML = "<thead> <tr> <th>Term</th> <th>Definition</th> <th>Actions</th> </tr> </thead> <tbody> <tr> <td> <div class='flex'> <button onclick='buttons.edit.add();'> <i class='nf nf-oct-plus'></i> Add row </button> <button id='buttonEditDone' class='alt' onclick='buttons.edit.done()' > <i class='nf nf-oct-check'></i> Done </button> </div> </td> <td></td> <td></td> </tr> </tbody>"
-      /* the string above has the inner html of the table found in dashboard.html */
+      /* IMPORTANT: the string above has the inner html of the table found in dashboard.html */
+      /*
+        the same problem described and fixed above happens in the other table (the view only one under the flashcards),
+        the other table's problem is fixed below
+      */
+      document.getElementById("mainActionsTable") = "<thead> <tr> <th>Term</th> <th>Definition</th> </tr> </thead> <tbody> <tr> <td> <button onclick='buttons.open.edit();'> Edit </button> </td> <td></td> </tr> </tbody>"
+      /* IMPORTANT: the string above has the inner html of the OTHER table found in dashboard.html */
     },
   },
   save: {},
