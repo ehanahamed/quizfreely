e5a319af1f581e273c950cb5eb3961447f5c6b86
Ehan Ahamed <ehanahamed@ehan.dev>
2023-07-23 10:42:56 -0400

    Fixed conflicting variable assignment

--- site/js/supabase.js
+++ site/js/supabase.js
@@ -4,9 +4,6 @@ const supabaseClient = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZWpxa2lzb2x4cWdrZ3d3bGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU3NDE1MTgsImV4cCI6MjAwMTMxNzUxOH0.T8f_3fPAYnWdzguPonQZCAFmbAN-mTLQB3_EW9pjkis"
 );
 
-var newUserUi = [];
-var userUi = [];
-
 supabaseClient.auth.getSession().then(function (result) {
   if (result.data.session === null) {
     for (var i = 0; i < newUserUi.length; i++) {
