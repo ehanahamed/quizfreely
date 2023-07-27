b99cb2d6dca98e511d0286e5ffeadfe40daab08b
Ehan Ahamed <ehanahamed@ehan.dev>
2023-07-26 23:20:51 -0400

    Major code rearranging - new ui.elements.states.etc - new ui.states.etc(isState) - moved hiding and showing from supbase.js to ui.states.newUser() - put newUser/user elements under ui.elements.states.newUser

--- site/js/supabase.js
+++ site/js/supabase.js
@@ -6,18 +6,8 @@ const supabaseClient = createClient(
 
 supabaseClient.auth.getSession().then(function (result) {
   if (result.data.session === null) {
-    for (var i = 0; i < newUserUi.length; i++) {
-      document.getElementById(newUserUi[i]).classList.remove("hide");
-    }
-    for (var i2 = 0; i2 < userUi.length; i2++) {
-      document.getElementById(userUi[i2]).classList.add("hide");
-    }
+    ui.states.newUser(true);
   } else if (result.data.session !== null) {
-    for (var i3 = 0; i3 < newUserUi.length; i3++) {
-      document.getElementById(newUserUi[i3]).classList.add("hide");
-    }
-    for (var i4 = 0; i4 < userUi.length; i4++) {
-      document.getElementById(userUi[i4]).classList.remove("hide");
-    }
+    ui.states.newUser(false);
   }
 });
