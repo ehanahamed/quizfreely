26cc42f88fe5ea704961a6ce4b8c4063524b6574
Ehan Ahamed <ehanahamed@ehan.dev>
2023-08-01 05:20:57 +0000

    Only check condition if object is defined

--- site/js/supabase.js
+++ site/js/supabase.js
@@ -11,7 +11,7 @@ const supabaseClient = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZWpxa2lzb2x4cWdrZ3d3bGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU3NDE1MTgsImV4cCI6MjAwMTMxNzUxOH0.T8f_3fPAYnWdzguPonQZCAFmbAN-mTLQB3_EW9pjkis"
 );
 
-if (states) {
+if (typeof states !== "undefined") {
   supabaseClient.auth.getSession().then(function (result) {
     if (result.data.session === null) {
       states.newUser(true);
