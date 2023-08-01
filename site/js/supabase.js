5dc760554cfe02a9918ea44e438b0a5e33769816
Ehan Ahamed <ehanahamed@ehan.dev>
2023-08-01 01:16:43 -0400

    Only run function if function exists

--- site/js/supabase.js
+++ site/js/supabase.js
@@ -11,10 +11,12 @@ const supabaseClient = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rZWpxa2lzb2x4cWdrZ3d3bGJ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODU3NDE1MTgsImV4cCI6MjAwMTMxNzUxOH0.T8f_3fPAYnWdzguPonQZCAFmbAN-mTLQB3_EW9pjkis"
 );
 
-supabaseClient.auth.getSession().then(function (result) {
-  if (result.data.session === null) {
-    states.newUser(true);
-  } else if (result.data.session !== null) {
-    states.newUser(false);
-  }
-});
+if (states) {
+  supabaseClient.auth.getSession().then(function (result) {
+    if (result.data.session === null) {
+      states.newUser(true);
+    } else if (result.data.session !== null) {
+      states.newUser(false);
+    }
+  });
+}
