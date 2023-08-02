84387550c60f7ab40540135836123ba1a7d122b8
Ehan Ahamed <ehanahamed@ehan.dev>
2023-08-02 01:54:25 -0400

    Changes relating to new email-less sign in

--- site/js/supabase.js
+++ site/js/supabase.js
@@ -47,19 +47,3 @@ function updateStudySet() {
     }
   )
 }
-
-/*
-Prompt user to create username if username is null
-*/
-supabaseClient.auth.getSession().then(
-  function (result) {
-    if (result.data.session !== null) {
-      if (result.data.session.user.user_metadata.hasOwnProperty("username") === false) {
-        var newDiv = document.createElement("div");
-        newDiv.id = "setUsername";
-        newDiv.innerHTML = "<div class='modal'><div class='content'><h2>Create a Username</h2><p>Finish setting up your account by setting your username.</p><a href='./setusername.html' class='button'>Set Username</button></div></div>"
-        document.body.appendChild(newDiv);
-      }
-    }
-  }
-);
