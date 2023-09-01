d1431c83c97f245688b49e2d146394fe6a0fae61
Ehan Ahamed <ehanahamed@ehan.dev>
2023-08-31 22:51:19 -0400

    Reupdate the update to update it fully

--- site/js/flashcards.js
+++ site/js/flashcards.js
@@ -1,6 +1,6 @@
 /*
   Quizfreely
-  Copyright (c) 2022 Quizfreely Contributors
+  Copyright (c) Ehan Ahamed and contributors
   Licensed under the UPL-1.0 License
   https://quizfreely.ehan.dev/LICENSE.txt
 */
