bd6c1a4f3c2e5c1d3adbb9d53df01ea4441cc334
Ehan <ehanahamed@ehan.dev>
2023-11-20 13:21:26 +0000

    Update site/js/pocketbase.js

--- site/js/pocketbase.js
+++ site/js/pocketbase.js
@@ -7,4 +7,19 @@
 
 var pocketbase = new PocketBase("http://127.0.0.1:8090")
 
+pocketbase.collection('users').authRefresh().then(
+  function (result) {
+    console.log(result);
+  }
+)
 
+if (typeof states !== "undefined") {
+  console.log(pocketbase.authStore.isValid());
+  /*.then(function (result) {
+    if (result.data.session === null) {
+      states.newUser(true);
+    } else if (result.data.session !== null) {
+      states.newUser(false);
+    }
+  });*/
+}
