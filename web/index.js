b87fcaa7535ed03c504301a59096e5934a19a5d1
Ehan <ehanahamed@ehan.dev>
2024-08-13 02:21:53 -0400

    reimplement error handling, 404s, pages, cookies, settings, and static assets, in fastify

--- web/index.js
+++ web/index.js
@@ -1,12 +1,19 @@
-import fastify from "fastify";
+import Fastify from "fastify";
 import fastifyView from "@fastify/view";
-import { Eta } from "eta";
+import fastifyCookie from "@fastify/cookie";
+import fastifyStatic from "@fastify/static";
 import path from "path";
+import { Eta } from "eta";
 import { themes } from "./themes.js";
 
 const port = 8080
 /* for prod: "quizfreely.com" */
 const domain = "localhost"
+
+
+const fastify = Fastify({
+  logger: true
+})
 
 const eta = new Eta({
   tags: [
@@ -17,53 +24,38 @@ const eta = new Eta({
   defaultExtension: ".html"
 })
 
-const server = fastify();
-
-server.register(fastifyView, {
+fastify.register(fastifyCookie);
+fastify.register(fastifyView, {
   engine: {
     eta
   },
-  templates: path.join(import.meta.dirname, "views"),
-})
-
-server.set_error_handler(
-  function (request, response, error) {
-    console.error(error);
-    response.status(500).send(
-      "500 Internal Server Error\n" +
-      ":( \n Report this at https://github.com/ehanahamed/quizfreely/issues"
-    )
-  }
-)
-
-server.set_not_found_handler(
-  function (request, response) {
-    response.status(404).type("html").send(
-      eta.render("404", pageData(request))
-    );
-  }
-)
-
-server.get("/assets/*", function (request, response) {
-  const path = request.path.replace("/assets", "");
-  const file = assets.get(path);
-
-  if (file === undefined) {
-    return response.status(404).send();
-  }
-
-  const filepath = file.path.split(".");
-  const ext = filepath[filepath.length - 1];
-
-  const content = file.content;
-  if (content instanceof Buffer) {
-    return response.type(ext).send(content);
-  } else {
-    return response.type(ext).stream(content);
+  root: path.join(import.meta.dirname, "views"),
+  defaultContext: {
+    themes: JSON.stringify(themes)
   }
 })
 
-function pageData(request) {
+fastify.register(fastifyStatic, {
+  root: path.join(import.meta.dirname, "assets"),
+  prefix: "/assets/"
+})
+
+fastify.setErrorHandler(function (error, request, reply) {
+  this.log.error(error)
+  reply.status(500).send(
+    "500 Internal Server Error\n" +
+    ":(\n" +
+    "Report this at https://github.com/ehanahamed/quizfreely/issues"
+  )
+})
+
+fastify.setNotFoundHandler(function (request, reply) {
+  reply.status(404).view("404.html", {
+    ...themeData(request)
+  })
+})
+
+function themeData(request) {
   let theme = "auto";
   if (request.cookies.theme !== undefined && themes.includes(request.cookies.theme)) {
     theme = request.cookies.theme;
@@ -73,95 +65,67 @@ function pageData(request) {
     themeCss = ""
   }
   return {
-    domain: domain,
-    themes: JSON.stringify(themes),
     theme: theme,
     themeCss: themeCss
   }
 }
 
-function page(page) {
-  return function (request, response) {
-    response.type("html").send(
-      eta.render(page, pageData(request))
-    )
-  }
-}
+fastify.get("/", function (request, reply) {
+  reply.view("home.html", {
+    ...themeData(request)
+  })
+});
+fastify.get("/home", function (request, reply) {
+  reply.view("home.html", {
+    ...themeData(request)
+  })
+});
+fastify.get("/settings", function (request, reply) {
+  reply.view("settings.html", {
+    ...themeData(request)
+  })
+});
+fastify.get("/sign-up", function (request, reply) {
+  reply.view("/account.html", {
+    signup: true,
+    ...themeData(request)
+  })
+})
 
-server.get("/", page("home"));
-server.get("/home", page("home"));
-server.get("/home/", page("home"));
-server.get("/settings", page("settings"));
-server.get("/settings/", page("settings"));
-function signupPage(request, response) {
-  response.type("html").send(
-    eta.render(
-      "account",
+fastify.get("/sign-in", function (request, reply) {
+  reply.view("/account.html", {
+    signup: false,
+    ...themeData(request)
+  })
+})
+
+fastify.get("/settings/themes/:theme", function (request, reply) {
+  if (themes.includes(request.params.theme)) {
+    let time = new Date();
+    /* 100 days * 24h * 60m * 60s = 8640000 sec for 100 days */
+    time.setSeconds(time.getSeconds() + 8640000)
+    reply.setCookie(
+      "theme",
+      request.params.theme,
       {
-        signup: true,
-        apiUrl: apiUrl,
-        apiPublicKey: apiPublicKey,
-        ...pageData(request)
+        domain: domain,
+        path: "/",
+        signed: false,
+        expires: time,
+        maxAge: 8640000,
+        httpOnly: true,
+        sameSite: "lax",
+        /* when secure is true,
+        browsers only send the cookie through https,
+        on localhost, browsers send it even if localhost isn't using https */
+        secure: true
       }
-    )
-  )
-}
-function loginPage(request, response) {
-  response.type("html").send(
-    eta.render(
-      "account",
-      {
-        signup: false,
-        apiUrl: apiUrl,
-        apiPublicKey: apiPublicKey,
-        ...pageData(request)
-      }
-    )
-  )
-}
-
-server.get("/signup", signupPage)
-server.get("/sign-up", signupPage)
-server.get("/login", loginPage)
-server.get("/log-in", loginPage)
-server.get("/signin", loginPage)
-server.get("/sign-in", loginPage)
-
-server.get(
-  "/user/:username",
-  function (request, response) {
-    response.send("hai, you typed " + request.path_parameters.username)
+    ).send("ok :3")
+  } else {
+    reply.status(400).send("invalid request body :(")
   }
-)
+})
 
-server.get(
-  "/settings/themes/:theme",
-  function (request, response) {
-    if (themes.includes(request.path_parameters.theme)) {
-      response.cookie(
-        "theme",
-        request.path_parameters.theme,
-        /* expire time in milliseconds,
-        100 days, 24hr per day, 60min per hour, 60sec per min, 1000 ms in a second */
-        100 * 24 * 60 * 60 * 1000,
-        {
-          domain: domain,
-          path: "/",
-          /* expire time in seconds,
-          100 days, 24hr/day, 60min/hour, 60sec/min */
-          maxAge: 100 * 24 * 60 * 60,
-          /* when secure is true,
-          browsers only send the cookie through https,
-          on localhost, browsers send it even if localhost isn't using https */
-          secure: true,
-          httpOnly: true,
-          sameSite: "lax"
-        }
-      ).send("ok :3")
-    } else {
-      response.status(400).send("invalid request body :(")
-    }
-  }
-)
-
-server.listen(port);
+fastify.listen({
+  port: port
+})
