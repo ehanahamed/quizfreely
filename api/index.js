4be180b0362d81100df5e004d0041b3136100872
Ehan <ehanahamed@ehan.dev>
2024-08-20 02:37:51 -0400

    implement cors, username regex check, password length, working account creation

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const Fastify = require("fastify");
 const fastifyCookie = require("@fastify/cookie");
+const fastifyCors = require("@fastify/cors");
 const fastifyPostgres = require("@fastify/postgres");
 const path = require("node:path");
 
 const port = process.env.PORT;
 const host = process.env.HOST;
-const pgConnection = process.env.POSTGRES_URI
+const pgConnection = process.env.POSTGRES_URI;
+const corsOrigin = process.env.CORS_ORIGIN;
 
 const fastify = Fastify({
   logger: {
@@ -15,29 +17,71 @@ const fastify = Fastify({
   }
 })
 
+fastify.register(fastifyCors, {
+  origin: corsOrigin
+})
 fastify.register(fastifyPostgres, {
   connectionString: pgConnection
 })
 
 fastify.post("/sign-up", function (request, reply) {
-  fastify.pg.query(
-    "insert into auth.users (username, encrypted_password, display_name) " +
-    "values ($1, crypt($2, gen_salt('bf')), $1)",
-    [request.body.username, request.body.password],
-    function (error, result) {
-      if (error) {
-        request.log.error(error)
-        reply.status(500).send({
-          "error": true
-        })
+  if (request.body && request.body.username && request.body.password) {
+    let username = request.body.username;
+    /* regex to check if username has letters or numbers (any alphabet) or dot, underscore, or dash */
+    if (/^[\p{L}\p{N}\p{M}._-]+$/u.test(username) && username.length >= 2 && username.length < 100) {
+      if (request.body.password.length >= 8) {
+        fastify.pg.query(
+          "insert into auth.users (username, encrypted_password, display_name) " +
+          "values ($1, crypt($2, gen_salt('bf')), $1) returning id",
+          [username, request.body.password],
+          function (error, result) {
+            if (error) {
+              request.log.error(error)
+              reply.status(500).send({
+                "error": true
+              })
+            } else {
+              console.log(result)
+              reply.send({
+                error: false,
+                data: {
+                  user: {
+                    id: result.rows[0].id,
+                    //workinprogress (2)
+                    username: username,
+                    display_name: username
+                  },
+                  session: {
+                  }
+                }
+              })
+            }
+          }
+        )
       } else {
-        console.log(result)
-        reply.send({
-          "error": false
+        reply.status(400).send({
+          error: {
+            type: "password-weak"
+          }
         })
       }
+    } else {
+      reply.status(400).send(
+        {
+          error: {
+            type: "username-invalid"
+          }
+        }
+      )
     }
-  )
+  } else {
+    /* else (password or username not in request.body) */
+    reply.status(400).send({
+      error: {
+        type: "fields-missing"
+      }
+    })
+  }
 })
 
 fastify.post("/sign-in", function (request, reply) {
@@ -58,15 +102,15 @@ fastify.post("/sign-in", function (request, reply) {
             "values ($1) returning token"
             // work in progress
           )
-        reply.send({
-          "error": false,
-          "data": {
-            user: result.rows[0],
-            session: {
-              token: ""
+          reply.send({
+            "error": false,
+            "data": {
+              user: result.rows[0],
+              session: {
+                token: ""
+              }
             }
-          }
-        })
+          })
         } else {
 
         }
