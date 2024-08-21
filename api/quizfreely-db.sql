f5ddedcace814f08fdc0a207ef6d884dd2037023
Ehan <ehanahamed@ehan.dev>
2024-08-21 14:30:46 -0400

    move db setup file

--- api/quizfreely-db.sql
+++ api/quizfreely-db.sql
@@ -1,34 +1,21 @@
-Enable postgres extensions
-```sql
 create extension if not exists pgcrypto;
-```
 
-Create schema for auth
-```sql
 create schema auth;
-```
 
-Create auth.users table
-```sql
 create table auth.users (
   id bigint primary key generated always as identity,
   username text,
   encrypted_password text,
-  display_name text not null
+  display_name text not null,
+  unique (username)
 );
-```
 
-Create auth.sessions table
-```sql
 create table auth.sessions (
   token text not null primary key default encode(gen_random_bytes(32), 'base64'),
   user_id bigint not null,
   expire_at timestamptz default clock_timestamp() + '7 days'::interval
 );
-```
 
-Create public.studysets table
-```sql
 CREATE TABLE studysets (
   id bigint primary key generated always as identity,
   user_id bigint not null,
@@ -37,4 +24,3 @@ CREATE TABLE studysets (
   data jsonb not null,
   updated_at timestamptz not null
 );
-```
