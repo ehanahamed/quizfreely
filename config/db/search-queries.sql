32ef90e96b72681d5f0475e2e8d50bc9af25b985
Ehan <ehanahamed@ehan.dev>
2024-10-12 17:33:56 -0400

    use insert instead of copy

--- config/db/search-queries.sql
+++ config/db/search-queries.sql
@@ -1,61 +1,60 @@
-copy public.search_queries (query, subject) from stdin with (delimiter ',');
-'algebra','math/algebra'
-'biology','sci/bio'
-'bitwise operations','sci/compsci'
-'chemistry','sci/chem'
-'chinese','lang/中文'
-'calculus','math/calc'
-'cells','sci/bio'
-'cells and organelles','sci/bio'
-'charges of elements','sci/chem'
-'covalent bonds','sci/chem'
-'cardiovascular system','sci/bio'
-'circulatory system','sci/bio'
-'carbon cycle','sci/bio'
-'discrete math','math/discrete-math'
-'english','LA/english'
-'economics','SS/economics'
-'elements','sci/chem'
-'element names and symbols','sci/chem'
-'element charges','sci/chem'
-'french','lang/french'
-'geography','SS/geography'
-'geometry','math/geometry'
-'greek','lang/ελληνικά'
-'german','lang/deutsch'
-'history','SS/history'
-'irregular verbs','lang'
-'italian','lang/italiano'
-'inglés','lang/english'
-'japanese','lang/日本語'
-'korean','lang/한국어'
-'latin','lang/latin'
-'literary devices','LA'
-'medical terminology','sci/medsci'
-'mitosis','sci/bio'
-'number systems','sci/compsci'
-'nervous system','sci/bio'
-'organelles','sci/bio'
-'organic chemistry','sci/chem'
-'polyatomic ions','sci/chem'
-'precalculus','math/precalc'
-'physics','sci/physics'
-'psychology','sci/psychology'
-'quantum mechanics','sci'
-'respiratory system','sci/bio'
-'russian','lang/русский'
-'romeo and juliet','LA/english'
-'spanish','lang/español'
-'statistics','math/stat'
-'skeletal system','sci/bio'
-'trigonometry','math/geometry'
-'tagalog','lang/tagalog'
-'turkish','lang/türkçe'
-'us history','SS/history'
-'unit circle','math/geometry'
-'vocabulary','LA'
-'verbs','LA'
-'world history','SS/history'
-'world war 1','SS/history'
-'world war 2','SS/history'
-\.
+insert into public.search_queries (query, subject) values
+('algebra','math/algebra')
+('biology','sci/bio')
+('bitwise operations','sci/compsci')
+('chemistry','sci/chem')
+('chinese','lang/中文')
+('calculus','math/calc')
+('cells','sci/bio')
+('cells and organelles','sci/bio')
+('charges of elements','sci/chem')
+('covalent bonds','sci/chem')
+('cardiovascular system','sci/bio')
+('circulatory system','sci/bio')
+('carbon cycle','sci/bio')
+('discrete math','math/discrete-math')
+('english','LA/english')
+('economics','SS/economics')
+('elements','sci/chem')
+('element names and symbols','sci/chem')
+('element charges','sci/chem')
+('french','lang/french')
+('geography','SS/geography')
+('geometry','math/geometry')
+('greek','lang/ελληνικά')
+('german','lang/deutsch')
+('history','SS/history')
+('irregular verbs','lang')
+('italian','lang/italiano')
+('inglés','lang/english')
+('japanese','lang/日本語')
+('korean','lang/한국어')
+('latin','lang/latin')
+('literary devices','LA')
+('medical terminology','sci/medsci')
+('mitosis','sci/bio')
+('number systems','sci/compsci')
+('nervous system','sci/bio')
+('organelles','sci/bio')
+('organic chemistry','sci/chem')
+('polyatomic ions','sci/chem')
+('precalculus','math/precalc')
+('physics','sci/physics')
+('psychology','sci/psychology')
+('quantum mechanics','sci')
+('respiratory system','sci/bio')
+('russian','lang/русский')
+('romeo and juliet','LA/english')
+('spanish','lang/español')
+('statistics','math/stat')
+('skeletal system','sci/bio')
+('trigonometry','math/geometry')
+('tagalog','lang/tagalog')
+('turkish','lang/türkçe')
+('us history','SS/history')
+('unit circle','math/geometry')
+('vocabulary','LA')
+('verbs','LA')
+('world history','SS/history')
+('world war 1','SS/history')
+('world war 2','SS/history');
