31a06583a3cc95510e5943b49f28efc1a7821400
Ehan <ehanahamed@ehan.dev>
2024-10-27 11:07:54 -0400

    switch order of cron jobs

--- api/index.js
+++ api/index.js
@@ -1327,28 +1327,6 @@ fastify.listen({
     }
 })
 
-if (CRON_DELETE_EXPIRED_SESSIONS == "true") {
-new Cron(CRON_DELETE_EXPIRED_SESSIONS_INTERVAL, async function () {
-    try {
-        let client = await pool.connect();
-        try {
-            await client.query("BEGIN");
-            await client.query("set role quizfreely_auth");
-            await client.query("call auth.delete_expired_sessions()");
-            await client.query("COMMIT");
-            fastify.log.info("ran cron job for auth.delete_expired_sessions()")
-        } catch (error2) {
-            await client.query("ROLLBACK");
-            fastify.log.error(error2, "error while running cron job for auth.delete_expired_sessions()")
-        } finally {
-            client.release();
-        }
-    } catch (error) {
-        fastify.log.error(error, "error while connecting to pg pool client during cron job for auth.delete_expired_sessions()")
-    }
-});
-}
-
 if (CRON_CLEAR_LOGS == "true") {
     new Cron(CRON_CLEAR_LOGS_INTERVAL, async function () {
         try {
@@ -1361,6 +1339,28 @@ if (CRON_CLEAR_LOGS == "true") {
             )
         } catch (error) {
             fastify.log.error(error, "error while trying to clear logs with cron job")
+        }
+    });
+}
+
+if (CRON_DELETE_EXPIRED_SESSIONS == "true") {
+    new Cron(CRON_DELETE_EXPIRED_SESSIONS_INTERVAL, async function () {
+        try {
+            let client = await pool.connect();
+            try {
+                await client.query("BEGIN");
+                await client.query("set role quizfreely_auth");
+                await client.query("call auth.delete_expired_sessions()");
+                await client.query("COMMIT");
+                fastify.log.info("ran cron job for auth.delete_expired_sessions()")
+            } catch (error2) {
+                await client.query("ROLLBACK");
+                fastify.log.error(error2, "error while running cron job for auth.delete_expired_sessions()")
+            } finally {
+                client.release();
+            }
+        } catch (error) {
+            fastify.log.error(error, "error while connecting to pg pool client during cron job for auth.delete_expired_sessions()")
         }
     });
 }
