732a2d954c6b8624acbdae8720fa5dbb05ce073f
Ehan <ehanahamed@ehan.dev>
2024-12-08 22:44:44 -0500

    update indexeddb objectstores for studyset progress

--- web/assets/js/indexedDB.js
+++ web/assets/js/indexedDB.js
@@ -1,14 +1,19 @@
 function openIndexedDB(callback) {
-    var dbReq = window.indexedDB.open("quizfreelydata");
+    var dbReq = window.indexedDB.open("quizfreelydata", 2);
     dbReq.onerror = function (event) {
         console.error("IndexedDB no worky");
         alert("IndexedDB no worky :(");
+        console.error(dbReq.error);
     };
     dbReq.onupgradeneeded = function (event) {
         var db = event.target.result;
         if (db.objectStoreNames.contains("studysets") == false) {
           var studysetsObjectStore = db.createObjectStore("studysets", { keyPath: "id", autoIncrement: true });
           studysetsObjectStore.createIndex("title_idx", "title");
+        }
+        if (db.objectStoreNames.contains("scores") == false) {
+            var scoresObjectStore = db.createObjectStore("scores", { keyPath: "id", autoIncrement: true });
+            scoresObjectStore.createIndex("studyset_id_idx", "studyset_id")
         }
     };
     dbReq.onsuccess = function (event) {
