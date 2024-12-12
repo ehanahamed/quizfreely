b0acf4835b8e2e9523a1267e15fd4a67d5d3dc6d
Ehan <ehanahamed@ehan.dev>
2024-12-11 21:19:07 -0500

    update indexeddb object stores for review mode

--- web/assets/js/indexedDB.js
+++ web/assets/js/indexedDB.js
@@ -1,5 +1,5 @@
 function openIndexedDB(callback) {
-    var dbReq = window.indexedDB.open("quizfreelydata", 2);
+    var dbReq = window.indexedDB.open("quizfreelydata", 3);
     dbReq.onerror = function (event) {
         console.error("IndexedDB no worky");
         alert("IndexedDB no worky :(");
@@ -11,10 +11,14 @@ function openIndexedDB(callback) {
           var studysetsObjectStore = db.createObjectStore("studysets", { keyPath: "id", autoIncrement: true });
           studysetsObjectStore.createIndex("title_idx", "title");
         }
-        if (db.objectStoreNames.contains("scores") == false) {
+        if (db.objectStoreNames.contains("studysetprogress") == false) {
+            /* autoincrement is false, so we give the id for each record ourselves, because that id corresponds with the studyset id */
+            var scoresObjectStore = db.createObjectStore("studysetprogress", { keyPath: "studyset_id", autoIncrement: false });
+        }
+        /*if (db.objectStoreNames.contains("scores") == false) {
             var scoresObjectStore = db.createObjectStore("scores", { keyPath: "id", autoIncrement: true });
             scoresObjectStore.createIndex("studyset_id_idx", "studyset_id")
-        }
+        }*/
     };
     dbReq.onsuccess = function (event) {
         callback(event.target.result);
