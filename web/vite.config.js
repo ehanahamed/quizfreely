0820845593835d51db8cb72662c6d31b71c16eaa
Ehan <ehanahamed@ehan.dev>
2025-01-12 08:35:24 -0500

    add api proxy for development

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:8008",
+				changeOrigin: true,
+				rewrite: function (path) {
+					return path.replace(/^\/api/, "");
+				}
+			}
+		}
+	}
 });
