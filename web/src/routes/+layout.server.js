b22bb9d63fe0a78983e01e01fc0685960e7f511e
Ehan <ehanahamed@ehan.dev>
2025-01-17 18:40:23 -0500

    ✨ Page Transitions/Animations

---
+++ web/src/routes/+layout.server.js
@@ -0,0 +1,5 @@
+export async function load({ url }) {
+	return {
+		urlPathname: url.pathname
+	};
+};
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script>
 import Header from "$lib/components/Header.svelte";
 import Footer from "$lib/components/Footer.svelte";
-let { children } = $props();
+import { fade } from "svelte/transition";
+let { children, data } = $props();
 </script>
 
 <div style="min-height: 80vh">
     <Header />
-    {@render children()}
+    {#key data.urlPathname}
+    <div in:fade={{ duration: 250, delay: 250 }} out:fade={{ duration: 250 }}>
+        {@render children()}
+    </div>
+    {/key}
 </div>
 <Footer />
