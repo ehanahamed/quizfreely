e2073f2dd9ad4568ea4087c6df1de8cb9bd965fa
Ehan <ehanahamed@ehan.dev>
2025-01-22 19:05:03 +0000

    work in progress

---
+++ web/src/routes/studyset/edit-local/+page.server.js
@@ -0,0 +1,11 @@
+import fetchAuthData from '$lib/fetchAuthData.server'
+
+export async function load({ cookies, url }) {
+    let localId = url.searchParams.get("id");
+    let userResult = await fetchAuthData({ cookies })
+    return {
+        localId: localId,
+      authed: userResult.authed,
+      authedUser: userResult?.authedUser
+    }
+}
