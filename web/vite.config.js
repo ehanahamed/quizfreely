9246143f2fe7e58841540da4e7e66c634c599458
Ehan <ehanahamed@ehan.dev>
2025-01-30 05:04:22 +0000

    remove monicons

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,18 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import monicon from "@monicon/vite";
 
 export default defineConfig({
 	plugins: [
-		sveltekit(),
-		monicon({
-			icons: [
-				"simple-icons:codeberg",
-				"simple-icons:github",
-				"material-symbols:arrow-back-rounded",
-				"material-symbols:arrow-forward-rounded"
-			]
-		})
+		sveltekit()
 	],
 	server: {
 		port: process?.env?.PORT ?? 8080,
