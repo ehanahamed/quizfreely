5f161d651be5c96f59faf7bcd41ad2ebede391c3
Ehan <ehanahamed@ehan.dev>
2025-01-30 04:52:40 +0000

    add monicons vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import monicon from "@monicon/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		monicon({
+			icons: [
+				"simple-icons:codeberg",
+				"simple-icons:github",
+				"material-symbols:arrow-back-rounded",
+				"material-symbols:arrow-forward-rounded"
+			]
+		})
+	],
 	server: {
 		port: process?.env?.PORT ?? 8080,
 		proxy: {
