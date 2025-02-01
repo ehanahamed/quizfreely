57366612bd29ae8c1fcc4eb9baa7079c5dec3e6b
Ehan <ehanahamed@ehan.dev>
2025-02-01 17:15:51 -0500

    and also re-check in the page component

--- web/src/routes/search/+page.svelte
+++ web/src/routes/search/+page.svelte
@@ -19,27 +19,27 @@
 <main>
   <div class="grid page">
     <div class="content">
-      {#if (data.query) }
-      <p>Results for "{ data.query }"</p>
-      <div class="grid list" style="overflow-wrap:anywhere">
-      {#each data.results as studyset }
-      <div class="box">
-        <a href="/studysets/{ studyset.id }">
-          { studyset.title }
-        </a>
-        {#if (studyset.terms_count >= 1) }
-        <p class="h6">
-          { studyset.terms_count } Terms
-        </p>
-        {/if}
-      </div>
-      {/each}
-      </div>
+      {#if data.query?.length >= 1 }
+        <p>Results for "{ data.query }"</p>
+        <div class="grid list" style="overflow-wrap:anywhere">
+        {#each data.results as studyset }
+        <div class="box">
+          <a href="/studysets/{ studyset.id }">
+            { studyset.title }
+          </a>
+          {#if (studyset.terms_count >= 1) }
+          <p class="h6">
+            { studyset.terms_count } Terms
+          </p>
+          {/if}
+        </div>
+        {/each}
+        </div>
       {:else}
-      <h2 style="text-align:center;margin-top:4rem;margin-bottom:0px">Google 2</h2>
-      <p class="fg0" style="text-align:center;margin-top:0.2rem">The Sequel</p>
-      <Searchbar />
-      <div style="margin-bottom:20rem"></div>
+        <h2 style="text-align:center;margin-top:4rem;margin-bottom:0px">Google 2</h2>
+        <p class="fg0" style="text-align:center;margin-top:0.2rem">The Sequel</p>
+        <Searchbar />
+        <div style="margin-bottom:20rem"></div>
       {/if}
     </div>
   </div>
